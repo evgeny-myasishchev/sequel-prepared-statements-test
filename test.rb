86a8d055a037321fa1236f9f778c0583a0a96a6b
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-11-25 20:47:14 +0200

    Implementing actual test.

---
+++ test.rb
@@ -0,0 +1,42 @@
+gem 'pg'
+gem 'sequel'
+require 'yaml'
+require 'logger'
+require 'sequel'
+
+Log = Logger.new STDOUT
+
+def assert_data(expected, actual)
+  if actual <=> actual
+    Log.error "Wrong data persisted.\nExpected bytes: #{expected}\n  Actual: #{actual}"
+  else
+    Log.info 'Data persisted correctly'
+  end
+end
+
+db_config = YAML.load_file File.join(__dir__, 'database.yml')
+Log.info 'Database config loaded'
+
+db = Sequel.connect db_config
+db.loggers << Log
+db.sql_log_level = :debug
+
+db.create_table! :binary_store do |table|
+  primary_key :id
+  File :data
+end
+
+
+binary_data = []
+256.times { |b| binary_data << b }
+binary_store = db[:binary_store]
+
+Log.info 'Inserting using direct insert...'
+id = binary_store.insert data: Sequel.blob(binary_data.pack('C*'))
+assert_data binary_data, binary_store[id: id][:data].unpack('C*')
+
+binary_store.prepare(:insert, :insert_binary_data, { data: :$data })
+
+Log.info 'Inserting using prepared statement...'
+id = db.call(:insert_binary_data, data: Sequel.blob(binary_data.pack('C*')))
+assert_data binary_data, binary_store[id: id][:data].unpack('C*')
